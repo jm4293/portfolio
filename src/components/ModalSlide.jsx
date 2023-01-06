aa8ff6fb663a6330dd306028b4c56f60ab4708e9
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-06 23:47:57 +0900

    feat: 슬라이드 모달

---
+++ src/components/ModalSlide.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+function ModalSlide() {
+    return(
+        <Frame>
+            123
+        </Frame>
+    )
+}
+
+const Frame = styled.div`
+    width: 20vw;
+    height: 60vh;
+    /* background-color: beige; */
+    border: 1px solid black;
+    position: relative;
+    left: 107%;
+`;
+
+export default ModalSlide;
