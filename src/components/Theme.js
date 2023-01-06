e11a0cdc79bfee3e81daf806c4e0273253023561
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-06 23:50:20 +0900

    perf: 다크모드 수정

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,11 +1,19 @@
 export const darkTheme = {
-  color: '#ffffff',
-  bgColor: '#000000',
-  border: '1px solid white',
+  color: 'rgb(195, 195, 195)',
+  bgColor: 'rgb(43, 43, 43)',
+  ScrollTopColor: 'rgb(43, 43, 43)',
+  ScrollBottomColor: 'rgb(124, 124, 124)',
+  FooterIcon: 'rgb(195, 195, 195)',
+  ContentListbgColor: 'rgb(30, 31, 33)',
+  ContentListColor: 'rgb(255, 252, 255)'
 };
 
 export const lightTheme = {
-  color: '#000000',
-  bgColor: '#ffffff',
-  border: '1px solid black',
+  color: 'black',
+  bgColor: 'white',
+  ScrollTopColor: 'rgb(124, 124, 124)',
+  ScrollBottomColor: 'rgb(209, 209, 209)',
+  FooterIcon: 'rgb(7, 7, 7)',
+  ContentListbgColor: 'rgb(255, 255, 255)',
+  ContentListColor: 'rgb(0, 0, 0)'
 };
