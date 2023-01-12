79730e6176f914ddc1846ede30e4e193cc8748fa
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-12 21:07:56 +0900

    refactor: 부자연스러운 메뉴바 스크롤 내릴때 애니메이션 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,123 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { BiSun, BiMoon } from 'react-icons/bi';                 // 다크모드 아이콘 설정
 import ReorderIcon from '@mui/icons-material/Reorder';
+import ScrollProgress from './ScrollProgress';
 
 function Header({ isDarkMode, setIsDarkMode, isModalSlide, setIsModalSlide }) {
+    const [change, setChange] = useState(false);                    
+    const [scrollProg, setScrollprog] = useState(0);            // 스크롤 진행바 계산 값
+    let scrollref = useRef(null);
+
+    // 스크롤 값
+    const handler = useCallback(() => {
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+        if(window.scrollY > 80){
+            setChange(true)
+        }
+        else {
+            setChange(false);
+        }
+
+        // console.log(scrollTop)
+        // console.log(scrollHeight)
+        // console.log(clientHeight)
+        // console.log(scrollref.current.offsetTop)
+
+        const heightMax = scrollHeight - clientHeight;
+        const heightPercent = scrollTop / heightMax;
+
+        setScrollprog(heightPercent * 100);
+    }, [])
+
+    // 스크롤 값
+    useEffect(() => {
+        const timer = setInterval(() => {
+            window.addEventListener('scroll', handler);
+        }, 100);
+        return () => {
+            clearInterval(timer);
+            window.removeEventListener('scroll', handler);
+        }
+    }, [handler])
+
     return (
-        <Frame>
-            <DarkModeIconStyled onClick={() => setIsDarkMode(!isDarkMode)}>
-                {isDarkMode ? <BiMoon /> : <BiSun />}
-            </DarkModeIconStyled>
-            <div id="header" style={{ fontSize: '24px' }}>jm's portfolio</div>
-           <ReorderIconStyled onClick={() => setIsModalSlide(!isModalSlide)}/>
+        <Frame change={change}>
+            <Top change={change}>
+                <div style={{display: change ? "none" : "block"}}></div>
+                <div ref={scrollref} style={{ fontSize: '24px' }}>jm's portfolio</div>
+                <HiddenItem change={change} onClick={() => window.scrollTo(0, 120)}>HOME</HiddenItem>
+                <HiddenItem change={change} onClick={() => window.scrollTo(0, 290)}>ABOUT</HiddenItem>
+                <HiddenItem change={change} onClick={() => window.scrollTo(0, 580)}>PROJECT</HiddenItem>
+                <HiddenItem change={change} onClick={() => window.scrollTo(0, 2020)}>CAREER</HiddenItem>
+                <HiddenItem change={change} onClick={() => window.scrollTo(0, 2350)}>SKILLS</HiddenItem>
+                <DarkModeIcon onClick={() => setIsDarkMode(!isDarkMode)}>{isDarkMode ? <BiMoon /> : <BiSun />}</DarkModeIcon>
+            </Top>
+            <Bottom>
+                <ScrollProgress scrollProg={scrollProg} />
+            </Bottom>
         </Frame>
     )
 }
 
 const Frame = styled.div`
-    position: sticky;
-    top: 0;
-    height: 8vh;
+    /* position: absolute; */
+    /* top: 0; */
+    /* height: 200px; */
+    /* position: absolute; */
+    position: ${props => props.change ? "fixed" : "absolute"};
+    width: ${props => props.change ? 90 : 100}%;
+    /* top: 0; */
+    height: ${props => props.change ? 100 : 200}px;
+    /* width: 90%; */
+    /* border: 1px solid red; */
     background-color: ${props => props.theme.bgColor};
-    
     color: ${props => props.theme.color};
     display: flex;
-    justify-content: space-around;
-    align-items: center;
-    z-index: 100;
-    font-weight: 600;
+    flex-direction: column;
+    /* margin-bottom: 200px; */
+    /* border-bottom: 1px solid black; */
+
+    /* transition: height .1s; */
+    
+    /* z-index: 100; */
+    /* font-weight: 600; */
 `;
 
-const DarkModeIconStyled = styled.div`
+const Top = styled.div`
+    width: 100%;
+    /* height: 100px; */
+    /* position: absolute; */
+    height: 199px;
+    display: flex;
+    justify-content: space-around;
+    /* border: 1px solid blue; */
+    /* justify-content: ${props => props.change ? "space-between" : "space-around"}; */
+    align-items: center;
+    /* border: 1px solid black; */
+`;
+
+const DarkModeIcon = styled.div`
     cursor: pointer;
     transform: scale(1.4);
 
     &:hover{
-        transform: scale(1.8);
-        transition: transform .5s;
+        transform: scale(1.6);
+        transition: transform .3s;
     }
 `;
 
-const ReorderIconStyled = styled(ReorderIcon)`
-    cursor: pointer;
+const HiddenItem = styled.div`
+    display: ${props => props.change ? "block" : "none"};
+`;
 
-    &:hover{
-        transform: scale(1.2);
-        transition: transform .5s;
-    }
+const Bottom = styled.div`
+/* position: absolute; */
+    /* border: 1px solid blue; */
+    width: 100%;
+    height: 1px;
+    /* clear: both; */
 `;
 
 export default Header;
--- src/components/ScrollProgress.jsx
+++ src/components/ScrollProgress.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-function ScrollProgress({scrollProg, isModalSlide}) {
+function ScrollProgress({scrollProg}) {
     return (
-        <>
-            <Bottom isModalSlide={isModalSlide} >
+        <Frame>
+            <Bottom>
                 <Top style={{ width: scrollProg + '%' }} />
             </Bottom>
-        </>
+        </Frame>
     )
 }
 
+const Frame = styled.div`
+    width: 100%;
+    height: 100%;
+    /* position: fixed; */
+    /* border: 1px solid black */
+`;
 
 const Bottom = styled.div`
-    width: 100%;
+    /* width: 100%; */
     height: 2px;
-    right: ${props => props.isModalSlide ? 250 : 0}px;                  // 슬라이드 모달이 열리면 스크롤 진행 바도 밀리기 위해
+    /* right: ${props => props.isModalSlide ? 160 : 0}px;                  // 슬라이드 모달이 열리면 스크롤 진행 바도 밀리기 위해 */
     
     background-color: ${props => props.theme.ScrollProgressBottomColor};
-    position: fixed;
-    cursor: pointer;
-    /* z-index: 99; */
+    /* background-color: black; */
+    /* position: fixed; */
+    /* cursor: pointer; */
+    /* z-index: 100; */
 `;
 
 const Top = styled.div`
